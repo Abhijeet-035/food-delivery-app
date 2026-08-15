7956f574260366ee8f3e0b5aa8e5e2ad337ca294
Abhijeet Kumar <jsrabhi819@gmail.com>
2026-08-15 17:04:36 +0530

    changes in orderController file

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing user order from frontend
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://food-website-by-arunkumar.onrender.com";
+  const frontend_url = "https://food-delivery-app-iota-sandy.vercel.app";
   try {
     const newOrder = new Order({
       userId: req.body.userId,
